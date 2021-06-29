471febed4f6356ea8d183866aa487d9112d5454c
Ash Davis <sadavis0010@gmail.com>
2021-06-29 11:30:29 -0700

    added apm

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 const express = require('express');
+const Rollbar = require('rollbar');
 const path = require('path');
 const app = express();
+
+const rollbar = new Rollbar({
+    accessToken: 'f3c8166fe8624cb3aef294fca722ac0f',
+    captureUncaught: true,
+    captureUnhandledRejections: true,
+})
 
 app.use(express.json())
 
 app.get('/', function(req,res){
+    rollbar.log('Hello world!')
     res.sendFile(path.join(__dirname, '/public/index.html'))
     // whatever the directory is, join that path to the path in the string
 })
